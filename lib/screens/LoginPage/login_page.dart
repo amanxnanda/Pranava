f8c46a910dcdbf32d034d82bc5cab37909fd9ee6
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-02-01 22:48:56 +0530

    added signup button

--- lib/screens/LoginPage/login_page.dart
+++ lib/screens/LoginPage/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pranava/screens/LoginPage/image_bg.dart';
-import 'package:pranava/screens/LoginPage/text_field_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pranava/screens/LoginPage/Widgets/image_bg.dart';
+import 'package:pranava/screens/LoginPage/Widgets/text_field_widget.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key key}) : super(key: key);
@@ -16,10 +17,53 @@ class LoginPage extends StatelessWidget {
         children: [
           ImageBackground(),
           SafeArea(
-            child: TextFieldWidget(
-              emailController: emailController,
-              passwordController: passwordController,
-              nameController: nameController,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFieldWidget(
+                  emailController: emailController,
+                  passwordController: passwordController,
+                  nameController: nameController,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      // LoadingIndicator(
+                      //   isLoading: _isLoading,
+                      // ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.orange,
+                          fontFamily: 'Blauer',
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 60.0,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(22.0),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            color: Colors.white,
+                            focusColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                            icon: Icon(FontAwesomeIcons.arrowRight),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
